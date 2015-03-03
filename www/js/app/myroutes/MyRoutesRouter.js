a6efc4c0f489e11e8340f34519cc5f4b3ecb6254
Arun Gopalpuri <arun0009@gmail.com>
2015-03-03 13:06:58 -0800

    not caching my routes + work around for iOS select bug

--- www/js/app/myroutes/MyRoutesRouter.js
+++ www/js/app/myroutes/MyRoutesRouter.js
@@ -2,6 +2,7 @@ angular.module('bartionic.myroutes', ['ionic', 'ngResource'])
     .config(function ($stateProvider) {
         $stateProvider.state('tab.myroutes', {
             url: '/myroutes',
+            cache: false,
             views: {
                 'tab-myroutes': {
                     controller: 'MyRoutesCtrl as myroutes',
