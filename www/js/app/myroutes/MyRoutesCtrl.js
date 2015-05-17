356c60d88dbde3a61540365c8f4acb483e3febe2
Arun Gopalpuri <arun0009@gmail.com>
2015-05-17 14:59:18 -0700

    minor refactoring - added 2 functions to reduce my routes complexity
    minor refactoring, app uploaded to ionic view.

--- www/js/app/myroutes/MyRoutesCtrl.js
+++ www/js/app/myroutes/MyRoutesCtrl.js
@@ -1,13 +1,12 @@
 var MyRoutesCtrl = function ($scope, $state, $filter, $ionicPlatform, $q, $timeout, ScheduledDepartureDetailsService, EstTimeDepartureService) {
 
     $scope.myRoutes = [];
-    var scheduleDepartureDetailsPromises = [];
     var originNames = [];
     var destinationNames = [];
     //window.localStorage.clear();
-    this.loadFavoriteRouteSchedule = function () {
-        var favoriteRoutes = JSON.parse(window.localStorage.getItem('favoriteRoutes')) || [];
-        console.log('fav routes : ' + angular.toJson(favoriteRoutes));
+
+    function getScheduleDepuartureDetailsPromises(favoriteRoutes) {
+        var scheduleDepartureDetailsPromises = [];
         for (var i = 0; i < favoriteRoutes.length; i++) {
             var originAbbr = favoriteRoutes[i].originAbbr;
             var destinationAbbr = favoriteRoutes[i].destinationAbbr;
@@ -15,8 +14,42 @@ var MyRoutesCtrl = function ($scope, $state, $filter, $ionicPlatform, $q, $timeo
             destinationNames.push(favoriteRoutes[i].destinationName);
             scheduleDepartureDetailsPromises.push(ScheduledDepartureDetailsService.scheduledDepartureDetailsDeferredRequest(originAbbr, destinationAbbr).$promise);
         }
+        return scheduleDepartureDetailsPromises;
+    }
 
-        $q.all(scheduleDepartureDetailsPromises).then(function (data) {
+    function setEstimatedDepartureDetails(myRouteInfo, key, originAbbr, routeFare, destTimeMin, hasLink, trainHeadStation) {
+        EstTimeDepartureService.departureTimeDeferredRequest(originAbbr).promise.then(null, null, function (estTimeDeparture) {
+            //console.log("estTimeDeparture is : " + angular.toJson(estTimeDeparture));
+            myRouteInfo.id = key;
+            myRouteInfo.routeFare = routeFare;
+            myRouteInfo.destTimeMin = destTimeMin;
+            myRouteInfo.originName = originNames[key];
+            myRouteInfo.destinationName = destinationNames[key];
+            myRouteInfo.hasLink = hasLink;
+            var estDepartureDetails = $filter('filter')(estTimeDeparture.root.station.etd, {abbreviation: trainHeadStation}, true);
+            if (estDepartureDetails != null) {
+                //should be a way to filter and return first object?
+                if (angular.isArray(estDepartureDetails)) {
+                    estDepartureDetails = estDepartureDetails[0];
+                }
+                if (angular.isArray(estDepartureDetails.estimate)) {
+                    myRouteInfo.estDeparture = isNaN(estDepartureDetails.estimate[0].minutes) ? 'LEAVING_NOW' : parseInt(estDepartureDetails.estimate[0].minutes) * 60;
+                } else {
+                    myRouteInfo.estDeparture = isNaN(estDepartureDetails.estimate.minutes) ? 'LEAVING_NOW' : parseInt(estDepartureDetails.estimate.minutes) * 60;
+                }
+            }
+            //$scope.$broadcast('timer-set-countdown', myRouteInfo.estDeparture);
+            var myRouteInfoInScope = $filter('filter')($scope.myRoutes, {id: myRouteInfo.id}, true);
+            if (!myRouteInfoInScope.length) {
+                $scope.myRoutes.push(myRouteInfo);
+            }
+        });
+    }
+
+    this.loadFavoriteRouteSchedule = function () {
+        var favoriteRoutes = JSON.parse(window.localStorage.getItem('favoriteRoutes')) || [];
+        console.log('fav routes : ' + angular.toJson(favoriteRoutes));
+        $q.all(getScheduleDepuartureDetailsPromises(favoriteRoutes)).then(function (data) {
             angular.forEach(data, function (scheduledDepartureDetails, key) {
                 var trainHeadStation = "";
                 var myRouteInfo = {};
@@ -31,47 +64,22 @@ var MyRoutesCtrl = function ($scope, $state, $filter, $ionicPlatform, $q, $timeo
                 }
 
                 var originAbbr = favoriteRoutes[key].originAbbr;
-                EstTimeDepartureService.departureTimeDeferredRequest(originAbbr).promise.then(null, null, function (estTimeDeparture) {
-                    //console.log("estTimeDeparture is : " + angular.toJson(estTimeDeparture));
-                    myRouteInfo.id = key;
-                    myRouteInfo.routeFare = routeFare;
-                    myRouteInfo.destTimeMin = destTimeMin;
-                    myRouteInfo.originName = originNames[key];
-                    myRouteInfo.destinationName = destinationNames[key];
-                    myRouteInfo.hasLink = hasLink;
-                    var estDepartureDetails = $filter('filter')(estTimeDeparture.root.station.etd, {abbreviation: trainHeadStation}, true);
-                    if (estDepartureDetails != null) {
-                        //should be a way to filter and return first object?
-                        if (angular.isArray(estDepartureDetails)) {
-                            estDepartureDetails = estDepartureDetails[0];
-                        }
-                        if (angular.isArray(estDepartureDetails.estimate)) {
-                            myRouteInfo.estDeparture = isNaN(estDepartureDetails.estimate[0].minutes) ? 'LEAVING_NOW' : parseInt(estDepartureDetails.estimate[0].minutes) * 60;
-                        } else {
-                            myRouteInfo.estDeparture = isNaN(estDepartureDetails.estimate.minutes) ? 'LEAVING_NOW' : parseInt(estDepartureDetails.estimate.minutes) * 60;
-                        }
-                    }
-                    //$scope.$broadcast('timer-set-countdown', myRouteInfo.estDeparture);
-                    var myRouteInfoInScope = $filter('filter')($scope.myRoutes, {id: myRouteInfo.id}, true);
-                    if (!myRouteInfoInScope.length) {
-                        $scope.myRoutes.push(myRouteInfo);
-                    }
-                });
+                setEstimatedDepartureDetails(myRouteInfo, key, originAbbr, routeFare, destTimeMin, hasLink, trainHeadStation);
             });
         });
     }
 
-    this.deleteRoute = function(route){
+    this.deleteRoute = function (route) {
         var favoriteRoutes = JSON.parse(window.localStorage.getItem('favoriteRoutes')) || [];
         for (var i = 0; i < favoriteRoutes.length; i++) {
             console.log(favoriteRoutes[i]);
-            if(favoriteRoutes[i].originName === route.originName && favoriteRoutes[i].destinationName === route.destinationName){
+            if (favoriteRoutes[i].originName === route.originName && favoriteRoutes[i].destinationName === route.destinationName) {
                 favoriteRoutes.splice(i, 1);
             }
         }
         window.localStorage.setItem('favoriteRoutes', JSON.stringify(favoriteRoutes));
         window.location.reload();
-     }
+    }
 }
 
 
