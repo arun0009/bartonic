ea3a58fb9bd0b37fe5d2d869aefadb0921b7658a
Arun Gopalpuri <arun0009@gmail.com>
2016-01-11 21:06:33 -0800

    not caching Add Route view

--- www/js/app/addroute/AddRouteRouter.js
+++ www/js/app/addroute/AddRouteRouter.js
@@ -2,6 +2,7 @@ angular.module('bartonic.addroute', ['ionic', 'ngResource'])
     .config(function ($stateProvider) {
         $stateProvider.state('tab.addroute', {
             url: '/addroute',
+            cache: false,
             views: {
                 'tab-addroute': {
                     controller: 'AddRouteCtrl as addroute',
