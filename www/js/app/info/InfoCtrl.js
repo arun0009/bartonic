832d0cfc6849233f164d86a9d85b02dc5e1f6703
Arun Gopalpuri <arun.gopalpuri@capitalone.com>
2016-04-12 19:39:12 -0700

    updated css + advisory no delay message

--- www/js/app/info/InfoCtrl.js
+++ www/js/app/info/InfoCtrl.js
@@ -3,18 +3,14 @@ var InfoCtrl = function ($scope, $log, AdvisoryService) {
     AdvisoryService.advisoryObservable().subscribe(
         function (response) {
             $scope.advisoryTitle = "Service Advisories :"
-            if (angular.isDefined(response.data.root.bsa)) {
+            if (angular.isDefined(response.data.root.bsa) && angular.isDefined(response.data.root.bsa.type)) {
                 if (response.data.root.bsa.type === "DELAY" || response.data.root.bsa.type === "EMERGENCY") {
                     $scope.badgeCount = 1;
                     $scope.advisory = response.data.root.bsa.description.__cdata
                 }
             } else {
-                $scope.advisory = "All trains are running as per schedule."
+                $scope.advisory = "No delays reported."
             }
-        },
-        function (err) {
-            $scope.advisory = "All trains are running as per schedule."
-            $log.error(err);
         });
 }
 
